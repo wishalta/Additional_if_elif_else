cc9a4233a571083ea8ffcb0358e5c654e9db0505
wishalta <rolandas.lebed@gmail.com>
2024-10-10 14:27:59 +0300

    dock file first task

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-a = int(input('Kiek darbuotojas gali iskepti kepalu per valanda? '))
-b = int(input('Kiek darbuotoju turi kepykla? '))
-c = int(input(f'Vieno kepalo savikaina? '))
-d = int(input('Vieno kepalo pardavimo kaina? '))
-e = int(input('Kiek kepykla turi ta diena iskepti kepalu (uzsakymai)? '))
+import random
 
-dv = 8
 
-print(f'Kepykla per viena diena suspes iskepti {dv * a}')
-g = dv * a
-savikaina = g * c
-pelnas= d * g
-print(f'Pilna pelno dalys {pelnas - savikaina} Eur')
+# a = int(input('Kiek darbuotojas gali iskepti kepalu per valanda? '))
+# b = int(input('Kiek darbuotoju turi kepykla? '))
+# c = int(input(f'Vieno kepalo savikaina? '))
+# d = int(input('Vieno kepalo pardavimo kaina? '))
+# e = int(input('Kiek kepykla turi ta diena iskepti kepalu (uzsakymai)? '))
+#
+# dv = 8
+#
+# print(f'Kepykla per viena diena suspes iskepti {dv * a}')
+# g = dv * a
+# savikaina = g * c
+# pelnas= d * g
+# print(f'Pilna pelno dalys {pelnas - savikaina} Eur')
+#
+# if e < g:
+#     print('suspes iskepti')
+# elif e > g:
+#     print(f'Nesuspes iskepti {e - g} kepalu')
 
-if e < g:
-    print('suspes iskepti')
-elif e > g:
-    print(f'Nesuspes iskepti {e - g} kepalu')
+# 1
 
+# name = input('Jusu vardas: ')
+# surname = input('Jusu pavarde: ')
+# bornyear = int(input('Jusu gimimo metai: '))
+# yearnow = int(input('Kokie metai dabar: '))
+#
+# print(f'As esu {name} {surname}. Man yra {yearnow-bornyear} metu')
+
+# 2
+
+random.randint(0, 4)
